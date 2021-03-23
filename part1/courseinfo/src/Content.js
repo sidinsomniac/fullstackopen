381c8689ab1cabaa2c8aea95673d2da92bda2372
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-03-23 23:45:08 +0530

    1.2: course information, step2

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import Part from "./Part";
 
 const Content = props => {
     return (
         <>
-            <p>
-                {props.courseInfo.part1} {props.courseInfo.exercises1}
-            </p>
-            <p>
-                {props.courseInfo.part2} {props.courseInfo.exercises2}
-            </p>
-            <p>
-                {props.courseInfo.part3} {props.courseInfo.exercises3}
-            </p>
+            <Part part={props.courseInfo.part1} exercise={props.courseInfo.exercises1} />
+            <Part part={props.courseInfo.part2} exercise={props.courseInfo.exercises2} />
+            <Part part={props.courseInfo.part3} exercise={props.courseInfo.exercises3} />
         </>
     );
 };
