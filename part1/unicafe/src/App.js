c9b019422ac2dd9a0df8c4db9a1f59a45019f257
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-03-26 09:30:26 +0530

    1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Statistics from "./Statistics";
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="feedback">
+      <div>
+        <h1>give feedback</h1>
+        <div>
+          <button>good</button>
+          <button>neutral</button>
+          <button>bad</button>
+        </div>
+      </div>
+      <Statistics />
     </div>
   );
-}
+};
 
 export default App;
---
+++ part1/unicafe/src/Statistics.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Statistics = () => {
+    return <div>
+        <h1>statistics</h1>
+        <p>good 1</p>
+        <p>neutral 1</p>
+        <p>bad 1</p>
+        <p>all 1</p>
+        <p>average 1</p>
+        <p>positive 100%</p>
+    </div>;
+};
+
+export default Statistics;
