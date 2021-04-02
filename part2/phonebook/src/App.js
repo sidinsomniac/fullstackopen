9e9dff1822266099831fd618ba8ec3e4b78c1f29
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-04-02 08:11:43 +0530

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import AddPerson from "./components/AddPerson";
 import Search from "./components/Search";
 import Contacts from "./components/Contacts";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [searchVal, setSearchVal] = useState('');
 
-    const filteredPeople = persons.filter(person => {
-        if (person.name.toLowerCase().trim().includes(searchVal.toLowerCase().trim())) {
-            return person;
-        }
-    });
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response =>
+                setPersons(response.data)
+            );
+    }, []);
 
+    const filteredPeople = persons.filter(person =>
+        person.name.toLowerCase().trim().includes(searchVal.toLowerCase().trim())
+    );
 
     const addPerson = event => {
         event.preventDefault();
