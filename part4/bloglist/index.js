d869e5a8078ff31b9efd3e435be59da5128375a2
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-05-02 17:38:49 +0530

    Create backend test environment

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,26 +1,11 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const middleware = require("./utils/middleware");
+const app = require("./app");
 const config = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static("build"));
-app.use(middleware.requestLogger);
-app.use("/api/blogs/", blogsRouter);
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+const logger = require("./utils/logger");
+const http = require("http");
 
 const PORT = config.PORT;
+const server = http.createServer(app);
 
-console.log(mongoUrl, PORT);
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`);
 });
