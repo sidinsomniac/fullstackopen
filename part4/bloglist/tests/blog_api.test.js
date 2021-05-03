56a2765488a477d41e3bc25488611215f60e2d31
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-05-04 00:39:44 +0530

    Complete all tests for all api endpoints

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,7 +3,6 @@ const supertest = require("supertest");
 const mongoose = require("mongoose");
 const Blog = require("../models/blog");
 const { initialBlogs, blogsInDb } = require("./tests_helper");
-const blog = require("../models/blog");
 const api = supertest(app);
 
 beforeEach(async () => {
