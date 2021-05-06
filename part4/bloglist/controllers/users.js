a4571847d0f3ab742727f07c7cc42374ccee4f06
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-05-07 00:39:58 +0530

    4.17: bloglist expansion, step5

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 userRouter.get("/", async (request, response) => {
-    const users = await User.find({});
+    const users = await User.find({}).populate("blogs", { title: 1, url: 1, author: 1 });
     response.json(users);
 });
 
@@ -11,7 +11,7 @@ userRouter.post("/", async (request, response) => {
     const { body: { username, password, name } } = request;
     const saltRounds = 10;
 
-    if (password.length < 3) {
+    if (!password || password?.length < 3) {
         return response.status(400).json({ error: "User validation failed: password: Path `password` is shorter than the minimum allowed length (3)." });
     }
 
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    notes: [
+    blogs: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Blog"
