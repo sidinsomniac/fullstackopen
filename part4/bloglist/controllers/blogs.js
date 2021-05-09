a68248f0765b50594b75f2facb6a0413de8b6aa7
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-05-09 18:26:43 +0530

    4.19: bloglist expansion, step7

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = request => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+        return authorization.substring(7);
+    }
+    return null;
+};
 
 
 blogsRouter.get("/", async (request, response) => {
@@ -9,8 +18,14 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-    const { body: { title, author, url, likes, userId } } = request;
-    const user = await User.findById(userId);
+    const { body: { title, author, url, likes } } = request;
+    const token = getTokenFrom(request);
+    // eslint-disable-next-line no-undef
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: "token missing or invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
 
     const blog = new Blog({
         title, author, url, likes,
