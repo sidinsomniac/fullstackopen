0b4368242491c35c0e1b4a508a48779b475aa264
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-05-18 21:13:23 +0530

    5.1: bloglist frontend, step1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,80 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => {
+      console.log(blogs);
+      setBlogs(blogs);
+    }
+    );
+  }, []);
+
+  const handleLogin = async event => {
+    event.preventDefault();
+    try {
+      const currentUser = await loginService.login({ username, password });
+      setUser(currentUser);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label>
+          Username
+            <input type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password
+            <input type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+
+  const blogList = () => {
+    return (<>
+      <p>{user.name} has logged in</p>;
+      {
+        blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )
+      }
+    </>);
+  };
+
+
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+      <h2>Blogs</h2>
 
-export default App
+      {!user ? loginForm() : blogList()}
+
+
+    </div>
+  );
+};
+
+export default App;
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,27 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+let token = null;
 
-export default { getAll }
+const setToken = newToken => {
+  token = `bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+const createBlog = async newBlog => {
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  };
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
+
+const exportServices = { getAll, createBlog, setToken };
+
+export default exportServices;
