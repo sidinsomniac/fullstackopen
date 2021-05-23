492b6f41e6c1dff2ff569990a0931e3bbecd925a
CG1-Siddhartha <siddhartha.chatterjee@mondcloud.com>
2021-05-24 00:02:24 +0530

    5.9*: Blog list frontend, step9

--- part5/bloglist-frontend/src/components/Bloglist.js
+++ part5/bloglist-frontend/src/components/Bloglist.js
@@ -6,7 +6,7 @@ const Bloglist = ({ user, blogs, handleLogout, updateBlog }) => (
     <p>{user.name} has logged in <button onClick={handleLogout}>logout</button></p>
 
     {
-      blogs.map(blog =>
+      blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog updateBlog={updateBlog} key={blog.id} blog={blog} />
       )
     }
