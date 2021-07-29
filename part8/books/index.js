d6f3648d54cf98455f3226501bb64d043a783e02
sidinsomniac <chatterjeesid1993@gmail.com>
2021-07-29 23:26:46 +0530

    8.9: Books view

--- part8/books/index.js
+++ part8/books/index.js
@@ -4,25 +4,25 @@ const { v1: uuid } = require("uuid");
 let authors = [
     {
         name: 'Robert Martin',
-        id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+        id: "afa51ab0-344d-11e9-a414-719c6709cf3a",
         born: 1952,
     },
     {
         name: 'Martin Fowler',
-        id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+        id: "afa5b6f0-344d-11e9-a414-719c6709cf3b",
         born: 1963
     },
     {
         name: 'Fyodor Dostoevsky',
-        id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+        id: "afa5b6f1-344d-11e9-a414-719c6709cf3c",
         born: 1821
     },
     {
-        name: 'Joshua Kerievsky', // birthyear not known
-        id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+        name: 'Joshua Kerievsky',
+        id: "afa5b6f2-344d-11e9-a414-719c6709cf3d",
     },
     {
-        name: 'Sandi Metz', // birthyear not known
+        name: 'Sandi Metz',
         id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
     },
 ];
