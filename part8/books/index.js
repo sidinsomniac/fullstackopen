de29e60720e9990c6ba4089cc08287cbdb35c23a
sidinsomniac <chatterjeesid1993@gmail.com>
2021-08-02 18:46:10 +0530

    Add mongoose to library project backend

--- part8/books/index.js
+++ part8/books/index.js
@@ -1,5 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require("mongoose");
 const { v1: uuid } = require("uuid");
+
+const MONGODB_URI = 'mongodb+srv://sid:qwerty1234@fullstackopen.msbpa.mongodb.net/library?retryWrites=true&w=majority';
+
+console.log('connecting to', MONGODB_URI);
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message);
+    });
 
 let authors = [
     {
